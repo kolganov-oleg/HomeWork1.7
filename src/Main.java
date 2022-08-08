c553d791cfda167a37b8a867d0ad3632a936c5f9
kolgan <6775366@gmail.com>
2022-08-08 03:28:19 +0300

    1.7

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Задача 1
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.printf("ФИО сотрудника - %s\n", fullName);
+
+
+        // Задача 2
+
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + fullName.toUpperCase(Locale.ROOT));
+
+        // Задача 3
+
+        String name = "Иванов Семён СемЁнович";
+        System.out.println("Данные ФИО сотрудника —  " + name.replace("ё", "е").replace("Ё", "е"));
+
     }
 }
